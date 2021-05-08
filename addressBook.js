236e37a3f3194dd8e56a0ca1873b7fd290fc7291
amarprajapati99 <amarprajapati99@gmail.com>
2021-05-08 22:44:55 +0530

    Added : created address book contact

--- addressBook.js
+++ addressBook.js
@@ -1,1 +1,17 @@
+/* @Description - create contacts class and create constructor to used the this keyword
+ *for the initializing the instances set value with the possition */
+
+class Contact{
+
+constructor(params) {
+    this.firstName = params[0];
+    this.lastName = params[1];
+    this.address = params[2];
+    this.city = params[3];
+    this.state = params[4];
+    this.zip = params[5];
+    this.phoneNumber = params[6];
+    this.email = params[7];
+    }
+}
 console.log('Welcome to address book ')
